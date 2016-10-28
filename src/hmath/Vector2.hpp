a4b9d2ac56fccc86c35abf0923fd2b0a590266c9
Daniel Green <KasumiL5x@gmail.com>
2016-10-28 07:27:49 +0100

    Initial commit.

---
+++ src/hmath/Vector2.hpp
@@ -0,0 +1,72 @@
+#ifndef __hmath_Vector2__
+#define __hmath_Vector2__
+
+#include <array>
+#include <cmath>
+#include <cassert>
+
+namespace hmath {
+
+class Vector2 {
+public:
+	Vector2();
+	Vector2( const std::array<float, 2>& values );
+
+	// accessors
+	inline float const& operator[]( int index ) const;
+	inline float& operator[]( int index );
+	inline float x() const;
+	inline float y() const;
+	inline float z() const;
+
+	// comparison operators
+	inline bool operator==( const Vector2& vec ) const;
+	inline bool operator!=( const Vector2& vec ) const;
+
+	// tuple operations
+	void makeZero();
+	void makeUnit( int index );
+
+	static Vector2 Zero();
+	static Vector2 Unit( int index );
+
+protected:
+	std::array<float, 2> tuple_;
+};
+
+// unary operations
+Vector2 operator+( const Vector2& vec );
+Vector2 operator-( const Vector2& vec );
+
+// linear algebraic operations
+Vector2 operator+( const Vector2& v0, const Vector2& v1 );
+Vector2 operator-( const Vector2& v0, const Vector2& v1 );
+Vector2 operator*( const Vector2& vec, float scalar );
+Vector2 operator*( float scalar, const Vector2& vec );
+Vector2 operator/( const Vector2& vec, float scalar );
+Vector2& operator+=( Vector2& v0, const Vector2& v1 );
+Vector2& operator-=( Vector2& v0, const Vector2& v1 );
+Vector2& operator*( Vector2& vec, float scalar );
+Vector2& operator*( float scalar, Vector2& vec );
+Vector2& operator*=( Vector2& vec, float scalar );
+Vector2& operator/=( Vector2& vec, float scalar );
+
+// component algebraic operations
+Vector2 operator*( const Vector2& v0, const Vector2& v1 );
+Vector2 operator/( const Vector2& v0, const Vector2& v1 );
+Vector2& operator*=( Vector2& v0, const Vector2& v1 );
+Vector2& operator/=( Vector2& v0, const Vector2& v1 );
+
+// geometric operations
+float dot( const Vector2& v0, const Vector2& v1 );
+float length( const Vector2& vec );
+float lengthRobust( const Vector2& vec );
+float sqrLength( const Vector2& vec );
+float normalize( Vector2& vec );
+float normalizeRobust( Vector2& vec );
+
+#include "Vector2.inc"
+
+}
+
+#endif
---
+++ test/Vector2Test.cpp
@@ -0,0 +1,14 @@
+#include "CppUnitTest.h"
+#include <hmath/Vector2.hpp>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace hmath_test {
+
+TEST_CLASS(Vector2Test) {
+public:
+	TEST_METHOD(EmptyConstructor) {
+	}
+};
+
+}

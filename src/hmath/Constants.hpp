ad0a5f3520f4d07060f8355fb797fa72fd2398b9
Daniel Green <KasumiL5x@gmail.com>
2016-11-06 22:46:26 +0000

    Added deg/rad conversion constants.

--- src/hmath/Constants.hpp
+++ src/hmath/Constants.hpp
@@ -5,6 +5,12 @@ namespace hm {
 
 const static float Epsilon = 1e-7f;
 
+/* Multiply to convert degrees to radians. */
+const static float DegToRad = 0.0174532924f;
+
+/* Multiple to convert radians to degrees. */
+const static float RadToDeg = 57.29578f;
+
 }
 
 #endif

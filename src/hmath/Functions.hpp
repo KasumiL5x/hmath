fa42c99bb67d3e308b62c64ad74795cee334cfa4
Daniel Green <KasumiL5x@gmail.com>
2016-11-07 07:36:10 +0000

    Added clamp/wrapAngle functions.

--- src/hmath/Functions.hpp
+++ src/hmath/Functions.hpp
@@ -1,16 +1,22 @@
 #ifndef __hmath_Functions__
 #define __hmath_Functions__
 
+#include <cmath>
+
 namespace hm {
 
-inline float clamp01( float val ) {
-	if( val < 0.0f ) {
-		return 0.0f;
+inline float clamp( float val, float min, float max ) {
+	if( val < min ) {
+		return min;
 	}
-	if( val > 1.0f ) {
-		return 1.0f;
+	if( val > max ) {
+		return max;
 	}
 	return val;
+}
+
+inline float clamp01( float val ) {
+	return clamp(val, 0.0f, 1.0f);
 }
 
 inline float minimum( float a, float b ) {
@@ -19,6 +25,10 @@ inline float minimum( float a, float b ) {
 
 inline float maximum( float a, float b ) {
 	return (a > b) ? a : b;
+}
+
+inline float wrapAngle( float angle, float min, float max ) {
+	return fmodf(fmodf(angle - min, max - min) + (max - min), max - min) + min;
 }
 
 }
